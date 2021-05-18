32c0caade50ea10ff88e35580d81cd2a46b240c3
José Luiz Rodrigues <zeluizr@me.com>
2021-05-18 12:54:22 -0400

    analitucs

--- index.php
+++ index.php
@@ -518,6 +518,14 @@
 
 	<!-- Javascript -->
 	
-	
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-CGDVX893TY"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-CGDVX893TY');
+</script>
 </body>
 </html>
