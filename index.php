99a4340007868173159fd8eb3bf736bf66850682
Jose Luiz Rodrigues <jrodrigues@webimpacto.es>
2024-02-12 15:17:15 +0100

    Add Google tag for analytics tracking

--- index.php
+++ index.php
@@ -31,6 +31,19 @@
 	<script src="owlCarousel/owl.carousel.min.js"></script>
 	<script src="owlCarousel/owl-carousel-slide.js"></script>
 	<script src="owlCarousel/owl-carousel-nav.js"></script>
+
+	<!-- Google tag (gtag.js) -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-SMJZGY1SFR"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+
+		function gtag() {
+			dataLayer.push(arguments);
+		}
+		gtag('js', new Date());
+
+		gtag('config', 'G-SMJZGY1SFR');
+	</script>
 </head>
 
 <body>
